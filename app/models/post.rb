cf76ea25f193f11a143c1c27e6d88245ebfbcbe4
Ashley Wong <tofudragon333@gmail.com>
2022-08-16 09:29:01 -0400

    commit tree bb7c4ebef06aedb1a059506bd0ff094f874b430d

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ApplicationRecord
+    validates :title, :content, :summary, :category, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction, Non-Fiction) }
+    validate :must_be_clickbaity
+    def must_be_clickbaity
+        validates :content, inclusion: { in: %w("Won't Believe", "Secret", "Top", "guess")}
+    end
 end
